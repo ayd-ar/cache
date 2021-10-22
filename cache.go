188e49eddf9c92afde7ab104bc0bc4c97f2af310
Arsen <arsenaydarti@gmail.com>
2021-10-22 18:52:09 +0300

    added implementation Time-To-Live

--- cache.go
+++ cache.go
@@ -1,5 +1,10 @@
 package cache
 
+import (
+	"errors"
+	"time"
+)
+
 type Cache struct {
 	item map[string]interface{}
 }
@@ -10,12 +15,20 @@ func New() *Cache {
 	}
 }
 
-func (c *Cache) Set(key string, value interface{}) {
+func (c *Cache) Set(key string, value interface{}, ttl time.Duration) {
 	c.item[key] = value
+	go func() {
+		time.Sleep(ttl)
+		delete(c.item, key)
+	}()
 }
 
-func (c *Cache) Get(key string) interface{} {
-	return c.item[key]
+func (c *Cache) Get(key string) (interface{}, error) {
+	if value, ok := c.item[key]; ok {
+		return value, nil
+	}
+
+	return nil, errors.New("no such key")
 }
 
 func (c *Cache) Delete(key string) {
